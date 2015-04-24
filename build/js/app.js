4b4bbb30283fdde222cabf76051872c401d0fe49
Donald McKendrick <ddmckendrick@gmail.com>
2015-04-24 13:58:39 +0100

    update to white, slimmer nav

--- build/js/app.js
+++ build/js/app.js
@@ -1,7 +1,6 @@
 var app = angular.module('App', ['infinite-scroll', 'ngSanitize', 'btford.markdown', 'ui.router', 'ng-token-auth', 'ipCookie', 'ngStorage', 'angularPayments', 'btford.modal', 'akoenig.deckgrid', 'selectize']);
-var backendUrl = "http://localhost:3000/";
+var backendUrl = "https://www.shopshopgo.com/";
 var assetsUrl = 'http://localhost:9000/';
-var backendUrl = 'http://localhost:3000/';
 var scraperUrl = 'http://localhost:5000/';
 Stripe.setPublishableKey('pk_test_mfQJDA4oT57DLFi7l0HYu782');
 
